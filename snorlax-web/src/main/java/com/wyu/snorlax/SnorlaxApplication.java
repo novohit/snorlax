534e62d7529da3ff2d622f6225cd7feb833d4991
novo <zwx_info@163.com>
2023-03-29 19:03:51 +0800

    feat: SpringBoot框架搭建

---
+++ snorlax-web/src/main/java/com/wyu/snorlax/SnorlaxApplication.java
@@ -0,0 +1,15 @@
+package com.wyu.snorlax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author novo
+ * @since 2023-03-29
+ */
+@SpringBootApplication
+public class SnorlaxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SnorlaxApplication.class, args);
+    }
+}
